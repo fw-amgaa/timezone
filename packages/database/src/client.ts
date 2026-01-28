1e741155f48d8e311fe420a6aee765f3c79ad5c8
fw-amgaa <jasqwe6008@gmail.com>
2026-01-28 22:42:48 +0800

    added env to turb.json

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -4,16 +4,14 @@ import postgres from "postgres";
 import * as schema from "./schema";
 
 // Create the connection
-// const connectionString = process.env.DATABASE_URL!;
+const connectionString = process.env.DATABASE_URL;
 
-// if (!connectionString) {
-//   throw new Error("DATABASE_URL environment variable is not set");
-// }
+if (!connectionString) {
+  throw new Error("DATABASE_URL environment variable is not set");
+}
 
 // For query purposes
-const queryClient = postgres(
-  "postgresql://postgres.syegehgefqrlflmoxoyg:6j53flH5YIwj51eQ@aws-1-ap-northeast-2.pooler.supabase.com:6543/postgres"
-);
+const queryClient = postgres(connectionString);
 
 // Create drizzle instance with schema
 export const db = drizzle(queryClient, { schema });
